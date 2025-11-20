9760812df7d1e65f9326795f321bdfe5a9c90cd2
Yilmer Hernandez Camargo <yilmerpdf@gmail.com>
2025-11-20 09:44:18 -0500

    "Agregar Activida 4"

---
+++ semana-04/java/src/ConsultingProject.java
@@ -0,0 +1,17 @@
+ public class ConsultingProject {
+        private String projectId;
+        private String projectArea;
+        private double projectBudget;
+
+        public ConsultingProject(String projectId, String projectArea, double projectBudget) {
+            this.projectId = projectId;
+            this.projectArea = projectArea;
+            this.projectBudget = projectBudget;
+        }
+
+        public void ShowProjectInfo() {
+            System.out.println("Project's Id: " + projectId);
+            System.out.println("Project's Area: " + projectArea);
+            System.out.println("Project's budget: " + projectBudget + " dollars.");
+        }
+    }
